3bbd33e676b40b077e24932e0824888f231443e5
Seokmin <ghdtjrald240@gmail.com>
2024-05-27 00:06:12 +0900

    Fix type

--- packages/notion.tsx/src/primitives/property.ts
+++ packages/notion.tsx/src/primitives/property.ts
@@ -2,11 +2,15 @@ import { PropertyElement } from '../types/properties.ts';
 
 import Properties from './properties/index.ts';
 
-function Property<T extends keyof typeof Properties>({
+type PropertyProps<T extends keyof typeof Properties> = Parameters<
+  (typeof Properties)[T]
+>[0] & {
+  type: T;
+};
+
+export default function Property<T extends keyof typeof Properties>({
   type,
   ...props
-}: { type: T } & Parameters<(typeof Properties)[T]>[0]): PropertyElement {
+}: PropertyProps<T>): PropertyElement {
   return Properties[type](props as never);
 }
-
-export default Property;
